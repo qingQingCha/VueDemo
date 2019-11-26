65a817cfc504ba467ea7fbf7e83fba8b3a62c806
qingQingCha <15030081046@163.com>
2019-11-26 23:52:22 +0800

    首页样式

--- gshop/src/main.js
+++ gshop/src/main.js
@@ -3,11 +3,9 @@
  */
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
